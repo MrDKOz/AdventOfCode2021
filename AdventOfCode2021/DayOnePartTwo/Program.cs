502a18017a3bc715749a6403e2b637d1d2705aed
MrDKOz <daniel.osborne@pm.me>
2021-12-03 15:26:36 +0000

    Everything up until DayThreePartTwo

---
+++ AdventOfCode2021/DayOnePartTwo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DayOnePartTwo
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var inputs = System.IO.File.ReadAllLines("input.txt");
+
+            var previousValue = 0;
+            var depthIncreaseCount = 0;
+            var firstValueDone = false;
+
+            for (var i = 0; i <= inputs.Length - 3; i++)
+            {
+                var calculated = Calculate(inputs[i], inputs[i + 1], inputs[i + 2]);
+
+                if (firstValueDone)
+                {
+                    depthIncreaseCount += calculated > previousValue ? 1 : 0;
+                }
+                else
+                {
+                    firstValueDone = true;
+                }
+
+                previousValue = calculated;
+            }
+
+            Console.WriteLine($"Depth increased: {depthIncreaseCount} time(s)");
+        }
+
+        private static int Calculate(string val1, string val2, string val3)
+        {
+            return Convert.ToInt32(val1) + Convert.ToInt32(val2) + Convert.ToInt32(val3);
+        }
+    }
+}
---
+++ AdventOfCode2021/DayTwoPartTwo/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace DayTwoPartTwo
+{
+    public enum Directions
+    {
+        forward,
+        up,
+        down
+    }
+
+    public class Instruction
+    {
+        public readonly Directions Direction;
+        public readonly int Value;
+
+        public Instruction(Directions direction, int value)
+        {
+            Direction = direction;
+            Value = value;
+        }
+    }
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            var inputs = System.IO.File.ReadAllLines("input.txt");
+
+            var aim = 0;
+            var horizontal = 0;
+            var depth = 0;
+
+            foreach (var input in inputs)
+            {
+                var instruction = ConvertToInstruction(input);
+
+                switch (instruction.Direction)
+                {
+                    case Directions.forward:
+                        horizontal += instruction.Value;
+                        depth += aim * instruction.Value;
+                        break;
+                    case Directions.up:
+                        aim -= instruction.Value;
+                        break;
+                    case Directions.down:
+                        aim += instruction.Value;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            Console.WriteLine($"Answer: {horizontal * depth}");
+        }
+
+        /// Convert to string into its two parts, and return them.
+        private static Instruction ConvertToInstruction(string input)
+        {
+            var split = input.Split(" ");
+
+            if (Enum.TryParse<Directions>(split[0], out var direction) &&
+                int.TryParse(split[1], out var value))
+            {
+                return new Instruction(direction, value);
+            }
+
+            throw new Exception("Direction/Value was not valid");
+        }
+    }
+}
