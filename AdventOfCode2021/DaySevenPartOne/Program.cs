45335b8ed965c0f93ca134bbde13359cdc4a3d40
MrDKOz <daniel.osborne@pm.me>
2021-12-07 16:55:54 +0000

    DaySevenPartOne and Two - Complete

---
+++ AdventOfCode2021/DaySevenPartOne/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DaySevenPartOne
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var input = File.ReadAllLines("input.txt");
+            var initialPositions = input.First().Split(',').Select(number => Convert.ToInt32(number)).ToList();
+
+            var maxPos = initialPositions.Max();
+
+            var costs = new List<int>();
+
+            for (var testPos = 0; testPos < maxPos; testPos++)
+            {
+                var posCost = initialPositions.Select(crabPos => crabPos > testPos
+                        ? crabPos - testPos
+                        : testPos - crabPos)
+                    .ToList();
+
+                costs.Add(posCost.Sum());
+            }
+
+            Console.WriteLine($"Lowest cost: {costs.Min()}");
+        }
+    }
+}
---
+++ AdventOfCode2021/DaySevenPartTwo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DaySevenPartTwo
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var input = File.ReadAllLines("input.txt");
+            var initialPositions = input.First().Split(',').Select(number => Convert.ToInt32(number)).ToList();
+
+            var maxPos = initialPositions.Max();
+
+            var costs = new List<long>();
+
+            for (var testPos = 0; testPos < maxPos; testPos++)
+            {
+                var posCost = initialPositions.Select(initialPosition => initialPosition > testPos
+                        ? ConvertCost(initialPosition - testPos)
+                        : ConvertCost(testPos - initialPosition))
+                    .ToList();
+
+                costs.Add(posCost.Sum());
+            }
+
+            Console.WriteLine($"Lowest cost: {costs.Min()}");
+        }
+
+        private static int ConvertCost(int cost)
+        {
+            var convertedCost = 0;
+
+            for (var i = 1; i <= cost; i++)
+            {
+                convertedCost += i;
+            }
+
+            return convertedCost;
+        }
+    }
+}
